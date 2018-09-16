fd6359d5ec3802bb8b143717c225ef3ac518a5ef
Степан Лопатин <firewolf@mail.ru>
2018-09-16 11:33:50 +0300

    Initial Commit

---
+++ MobiSnake/MainActivity.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections.Generic;
+using System.Timers;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Graphics.Drawables.Shapes;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+using Java.Lang;
+using MobiSnake.Resources.layout;
+using static Android.Views.GestureDetector;
+using System;
+using Math = System.Math;
+
+namespace MobiSnake
+{
+    [Activity(Label = "MobiSnake", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity, IOnGestureListener
+    {
+        GameArea gameArea;
+
+        static Timer timer = new Timer();
+
+        GestureDetector _gestureDetector;
+        const int SWIPE_DISTANCE_THRESHOLD = 100;
+        const int SWIPE_VELOCITY_THRESHOLD = 100;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            gameArea = new GameArea(this);
+            //SetContentView(Resource.Layout.Main);
+            //SetContentView(new GameArea(this));
+            SetContentView(gameArea);
+
+            _gestureDetector = new GestureDetector(this);
+
+            timer.Interval = 50;
+            timer.Elapsed += Draw;
+
+            timer.Start();
+
+            //var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            //SetActionBar(toolbar);
+            //ActionBar.Title = "MobiSnake";
+        }
+
+        public void Draw(object sender, ElapsedEventArgs e)
+        {
+            CheckState();
+            RunOnUiThread(() => gameArea.Draw());
+        }
+
+        private void CheckState()
+        {
+            if (gameArea.IsOffScreen(gameArea.snakeHead)) Lose();
+            if (gameArea.snakeBody.Count == gameArea.snakeMaxLength)
+            {
+                gameArea.foodTimerCount = 0;
+                Win();
+            }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.top_menus, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.menu_about:
+                    Toast.MakeText(this, ActionBar.TitleFormatted + " is a mobile snake game!", 
+                                   ToastLength.Short).Show();
+                    return true;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+
+        public bool OnDown(MotionEvent e)
+        {
+            return true;
+        }
+
+        public bool OnFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY)
+        {
+            float distanceX = e2.GetX() - e1.GetX();
+            float distanceY = e2.GetY() - e1.GetY();
+
+            if (Math.Abs(distanceX) > SWIPE_DISTANCE_THRESHOLD && Math.Abs(velocityX) > SWIPE_VELOCITY_THRESHOLD
+                || Math.Abs(distanceY) > SWIPE_DISTANCE_THRESHOLD && Math.Abs(velocityY) > SWIPE_VELOCITY_THRESHOLD)
+            {
+                if (Math.Abs(velocityX) > Math.Abs(velocityY) && Math.Abs(distanceX) > Math.Abs(distanceY))
+                {
+                    OnSwipeLeftRight(distanceX);
+                    return true;
+                }
+
+
+                else if (Math.Abs(velocityY) > Math.Abs(velocityX) && Math.Abs(distanceY) > Math.Abs(distanceX))
+                {
+                    OnSwipeUpDown(distanceY);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        internal void Lose()
+        {
+            gameArea.isWorking = true;
+            timer.Stop();
+            RunOnUiThread(() =>  Toast.MakeText(this, "You lose!",
+                ToastLength.Long).Show());
+        }
+
+        internal void Win()
+        {
+            gameArea.isWorking = true;
+            timer.Stop();
+            RunOnUiThread(() => Toast.MakeText(this, "You win!",
+                ToastLength.Long).Show());
+        }
+
+        void OnSwipeUpDown(float distanceY)
+        {
+            if (distanceY < 0) gameArea.MoveUp();
+            else gameArea.MoveDown();
+        }
+
+        void OnSwipeLeftRight(float distanceX)
+        {
+            if (distanceX < 0) gameArea.MoveLeft();
+            else gameArea.MoveRight();
+        }
+
+        public void OnLongPress(MotionEvent e) {}
+        public bool OnScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY)
+        {
+            if (Math.Abs(distanceX) > SWIPE_DISTANCE_THRESHOLD || Math.Abs(distanceY) > SWIPE_DISTANCE_THRESHOLD)
+            {
+                if (Math.Abs(distanceX) > Math.Abs(distanceY))
+                {
+                    OnSwipeLeftRight(distanceX);
+                    return true;
+                }
+
+
+                if (Math.Abs(distanceY) > Math.Abs(distanceX))
+                {
+                    OnSwipeUpDown(distanceY);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public void OnShowPress(MotionEvent e) {}
+        public bool OnSingleTapUp(MotionEvent e)
+        {
+            return false;
+        }
+
+        public override bool OnTouchEvent(MotionEvent e)
+        {
+            _gestureDetector.OnTouchEvent(e);
+            return false;
+        }
+    }
+}
---
+++ MobiSnake/Resources/layout/GameArea.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Graphics.Drawables.Shapes;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MobiSnake.Resources.layout
+{
+    [Activity(Label = "MobiSnake")]
+    public class GameArea : View
+    {
+        ShapeDrawable food;
+        internal ShapeDrawable snakeHead;
+        internal Dictionary<int, ShapeDrawable> snakeBody;
+        int size;
+        static Random rnd;
+        int speed;
+        int shiftX;
+        int shiftY;
+        public int foodTimerCount;
+        internal int snakeMaxLength;
+        int snakePartsCounter;
+        int snakeTail;
+
+        Canvas gameArea;
+
+        bool isStarted;
+        internal bool isWorking;
+
+        public GameArea(Context context) : base(context)
+        {
+            Init();
+        }
+
+        void Init()
+        {
+            size = 50;
+            rnd = new Random();
+            //speed = 8;
+            speed = 16;
+            snakeMaxLength = 10;
+            foodTimerCount = 99;
+            var foodPaint = new Paint();
+            foodPaint.SetARGB(255, 255, 0, 0);
+            foodPaint.SetStyle(Paint.Style.Fill);
+            
+            food = new ShapeDrawable(new OvalShape());
+            food.Paint.Set(foodPaint);
+
+            var snakePaint = new Paint();
+            snakePaint.SetARGB(255, 0, 0, 255);
+            snakePaint.SetStyle(Paint.Style.Fill);
+            
+            snakeHead = new ShapeDrawable(new OvalShape());
+            snakeHead.Paint.Set(snakePaint);
+
+            snakeBody = new Dictionary<int, ShapeDrawable>();
+        }
+
+        protected override void OnDraw(Canvas canvas)
+        {
+            isWorking = true;
+            gameArea = canvas;
+            
+            if (isStarted)
+            {
+                PlaceSnake();
+                PlaceFood();
+            }
+            else InitialPlaceSnake();
+            isWorking = false;
+        }
+
+        public void Draw()
+        {
+            if (isWorking) return;
+            Invalidate();
+        }
+
+        void PlaceFood()
+        {
+            if (++foodTimerCount == 100)
+            {
+                int xLeft = rnd.Next(gameArea.Width - size);
+                int xRight = xLeft + size;
+                int yTop = rnd.Next(gameArea.Height - size);
+                int yBottom = yTop + size;
+                food.SetBounds(xLeft, yTop, xRight, yBottom);
+                foodTimerCount = 0;
+            }
+
+            food.Draw(gameArea);
+        }
+
+        void PlaceSnake()
+        {
+            var bounds = snakeHead.Bounds;
+            bounds.Left += shiftX;
+            bounds.Right += shiftX;
+            bounds.Top += shiftY;
+            bounds.Bottom += shiftY;
+            snakeHead.SetBounds(bounds.Left, bounds.Top, bounds.Right, bounds.Bottom);
+            //snakeBody.Add(snakePartsCounter++, snakeHead);
+            var s = new ShapeDrawable(new OvalShape());
+            s.Paint.Set(snakeHead.Paint);
+            s.Bounds = snakeHead.Bounds;
+            snakeBody.Add(snakePartsCounter++, s);
+
+            if (snakePartsCounter == 10) snakePartsCounter = 0;
+            
+            if (IsCrossed(snakeHead, food))
+            {
+                if (snakeBody.Count == snakeMaxLength) foodTimerCount = 0;
+                foodTimerCount = 99;
+                PlaceFood();
+            }
+            else
+            {
+                snakeBody.Remove(snakeTail++);
+                if (snakeTail == 10) snakeTail = 0;
+            }
+
+            DrawSnake(snakeBody);
+        }
+
+        void InitialPlaceSnake()
+        {
+            int xLeft = gameArea.Width / 2 - size / 2;
+            int xRight = xLeft + size;
+            int yTop = gameArea.Height / 2 - size / 2;
+            int yBottom = yTop + size;
+            snakeHead.SetBounds(xLeft, yTop, xRight, yBottom);
+            snakeBody.Add(snakePartsCounter, snakeHead);
+            snakeTail = snakePartsCounter++;
+            DrawSnake(snakeBody);
+            isStarted = true;
+        }
+
+        void DrawSnake(Dictionary<int, ShapeDrawable> snake)
+        {
+            foreach (var s in snake.Values)
+                s.Draw(gameArea);
+            //TestPlaceSnake();
+        }
+
+        void TestPlaceSnake()
+        {
+            int xLeft = gameArea.Width / 2 - size / 2;
+            int xRight = xLeft + size;
+            int yTop = gameArea.Height / 2 - size / 2;
+            int yBottom = yTop + size;
+
+            var sPaint = new Paint();
+            sPaint.SetARGB(0, 255, 0, 255);
+            sPaint.SetStyle(Paint.Style.Fill);
+            var s = new ShapeDrawable(new OvalShape());
+            s.Paint.Set(sPaint);
+            s.SetBounds(xLeft, yTop, xRight, yBottom);
+            s.Draw(gameArea);
+        }
+
+        public void MoveLeft()
+        {
+            shiftX = -speed;
+            shiftY = 0;
+        }
+
+        public void MoveRight()
+        {
+            shiftX = speed;
+            shiftY = 0;
+        }
+
+        public void MoveUp()
+        {
+            shiftY = -speed;
+            shiftX = 0;
+        }
+
+        public void MoveDown()
+        {
+            shiftY = speed;
+            shiftX = 0;
+        }
+
+        bool IsCrossed(ShapeDrawable s, ShapeDrawable f)
+        {
+            return Math.Abs(s.Bounds.Left - f.Bounds.Left) < size &&
+                   Math.Abs(s.Bounds.Top - f.Bounds.Top) < size;
+        }
+
+        internal bool IsOffScreen(ShapeDrawable s)
+        {
+            return s.Bounds.Left < 0 || s.Bounds.Right > gameArea.Width ||
+                   s.Bounds.Top < 0 || s.Bounds.Bottom > gameArea.Height;
+        }
+    }
+}
